04bb7d7cbc5286e6e53a785b4a0ab0113627a5c6
Aryan-Dixit <aryandixit1007@gmail.com>
2026-06-11 12:40:26 +0530

    javascript-design-patterns

---
+++ components/Navbar.tsx
@@ -0,0 +1,79 @@
+"use client";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
+import { ArrowLeftIcon } from "lucide-react";
+interface Pattern {
+  id: number;
+  path: string;
+  name: string;
+}
+const patterns: Pattern[] = [
+  {
+    id: 1,
+    path: "/constructor-patterns",
+    name: "Constructor Patterns",
+  },
+  {
+    id: 2,
+    path: "/singleton-patterns",
+    name: "Singleton Patterns",
+  },
+  {
+    id: 3,
+    path: "/factory-patterns",
+    name: "Factory Patterns",
+  },
+  {
+    id: 4,
+    path: "/prototype-patterns",
+    name: "Prototype Patterns",
+  },
+  {
+    id: 5,
+    path: "/observer-patterns",
+    name: "Observer Patterns",
+  },
+  {
+    id: 6,
+    path: "/proxy-patterns",
+    name: "Proxy Patterns",
+  },
+];
+function Navbar() {
+  const pathname = usePathname();
+  const isHome = pathname === "/";
+  return (
+    <div className="mt-4 flex gap-2 flex-wrap">
+      {!isHome && <BackButton />}
+      {patterns.map((pattern) => (
+        <LinkButton key={pattern.id} pattern={pattern} />
+      ))}
+    </div>
+  );
+}
+
+export default Navbar;
+
+function LinkButton({ pattern }: { pattern: Pattern }) {
+  const pathname = usePathname();
+  const isActive = pathname === pattern.path;
+  return (
+    <Link href={pattern.path}>
+      <Button variant={isActive ? "default" : "secondary"}>
+        {pattern.name}
+      </Button>
+    </Link>
+  );
+}
+
+function BackButton() {
+  return (
+    <Link href="/">
+      <Button variant={"outline"}>
+        <ArrowLeftIcon className="w-4 h-4 mr-2" />
+        Back
+      </Button>
+    </Link>
+  );
+}
